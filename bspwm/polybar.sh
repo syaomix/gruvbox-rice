85be6f8a64012d3d9fbd461b92bd46dcf0beda39
syaomix <71218321+syaomix@users.noreply.github.com>
2025-11-08 15:35:42 +0300

    Add files via upload

--- bspwm/polybar.sh
+++ bspwm/polybar.sh
@@ -5,8 +5,15 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 
-polybar main &
-
 if [[ $(xrandr -q | grep 'HDMI-1 connected') ]]; then
+    polybar laptop &
     polybar monitor &
+else
+    polybar main &
 fi
+
+#polybar main &
+
+#if [[ $(xrandr -q | grep 'HDMI-1 connected') ]]; then
+#    polybar monitor &
+#fi
--- bspwm/xconf.sh
+++ bspwm/xconf.sh
@@ -1,13 +1,19 @@
 #! /bin/sh
 
 xrandr --output eDP-1 --set TearFree on &
-xrandr --output eDP-1 --primary &
-xrandr --output HDMI-1 --set TearFree on&
-xrandr --output HDMI-1 --above eDP-1 &
+xrandr --output HDMI-1 --primary &
+xrandr --output HDMI-1 --set TearFree on &
+xrandr --output HDMI-1 --left-of eDP-1 &
 
 xinput --set-prop 10 'libinput Accel Speed' 0 &
 xinput --set-prop 8 'libinput Accel Speed' -0.7 &
 
 setxkbmap -option grp:alt_shift_toggle us,ru &
 
-bspc monitor "eDP-1" -s "HDMI-1"
+
+if [[ $(xrandr -q | grep 'HDMI-1 connected') ]]; then
+    bspc monitor eDP-1 -d 5 6 7 8 
+    bspc monitor HDMI-1 -d 1 2 3 4
+else
+    bspc monitor eDP-1 -d 1 2 3 4 
+fi
